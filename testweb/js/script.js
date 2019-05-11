31024f9d512cf4b89e0f29d52e02cfe3983a510a
huethytran <48084545+huethytran@users.noreply.github.com>
2019-05-11 22:28:26 +0700

    Add file js

--- testweb/js/script.js
+++ testweb/js/script.js
@@ -10,6 +10,9 @@
         },
         600:{
             items:2,
+        },
+        1800:{
+          items: 3,
         }
     }
     });
@@ -77,59 +80,4 @@
     window.addEventListener("load", onload, false); 
 
  
-    $(document).ready(function() {
-    var $TABLE = $('#table');
-var $BUTTON = $('#export-btn');
-var $EXPORT = $('#export');
-
-$('.table-add').click(function () {
-  var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide');
-  $TABLE.find('table').append($clone);
-});
-
-$('.table-remove').click(function () {
-  $(this).parents('tr').detach();
-});
-
-$('.table-up').click(function () {
-  var $row = $(this).parents('tr');
-  if ($row.index() === 1) return; // Don't go above the header
-  $row.prev().before($row.get(0));
-});
-
-$('.table-down').click(function () {
-  var $row = $(this).parents('tr');
-  $row.next().after($row.get(0));
-});
-
-// A few jQuery helpers for exporting only
-jQuery.fn.pop = [].pop;
-jQuery.fn.shift = [].shift;
-
-$BTN.click(function () {
-  var $rows = $TABLE.find('tr:not(:hidden)');
-  var headers = [];
-  var data = [];
   
-  // Get the headers (add special header logic here)
-  $($rows.shift()).find('th:not(:empty)').each(function () {
-    headers.push($(this).text().toLowerCase());
-  });
-  
-  // Turn all existing rows into a loopable array
-  $rows.each(function () {
-    var $td = $(this).find('td');
-    var h = {};
-    
-    // Use the headers from earlier to name our hash keys
-    headers.forEach(function (header, i) {
-      h[header] = $td.eq(i).text();   
-    });
-    
-    data.push(h);
-  });
-  
-  // Output the result
-  $EXPORT.text(JSON.stringify(data));
-});
-    });
