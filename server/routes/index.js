6512466e61abebe91aeff2773fd6b1e154b2277a
ThaiNhatMinh <t.n.minh1997@gmail.com>
2019-06-15 19:30:27 +0700

    * Fix missing router

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,13 @@ const user = require("./User.js");
 const admin = require("./Admin.js");
 const apiuser = require("./APIUser.js");
 const apiadmin = require("./APIAdmin.js");
-
+const apipost = require("./APIPost.js");
+const post = require("./Post.js");
+const newpost = require("./NewPost.js");
 module.exports = (router) => {
     router.use('/user', user());
     router.use('/news', post());
+    router.use('/new',newpost());
     router.use('/api/post', apipost());
     router.use('/admin', admin());
     router.use('/api/user', apiuser());
