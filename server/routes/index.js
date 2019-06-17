0ffa33dc131a71f696bf9384d2570d3d19a1c734
huethytran <48084545+huethytran@users.noreply.github.com>
2019-06-17 17:08:13 +0700

    update routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,13 +5,17 @@ const apiadmin = require("./APIAdmin.js");
 const apipost = require("./APIPost.js");
 const post = require("./Post.js");
 const newpost = require("./NewPost.js");
+const mainpage = require("./MainPage.js");
+const postslist = require("./PostsList.js");
 module.exports = (router) => {
     router.use('/user', user());
-    router.use('/news', post());
-    router.use('/new',newpost());
+    router.use('/posts', postslist());
+    router.use('/postdetail', post());
     router.use('/api/post', apipost());
     router.use('/admin', admin());
     router.use('/api/user', apiuser());
+    router.use('/writter',newpost());
+    router.use('/news',mainpage());
     router.use('/api/admin', apiadmin());
     return router;
 }
